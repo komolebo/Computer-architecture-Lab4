abf28ae173deb051a0ebd1814284f48dc8e31680
kokopyka <kokopyka@gmail.com>
2015-06-19 15:03:51 +0300

    Update client.h

--- client/client.h
+++ client/client.h
@@ -5,7 +5,6 @@
 */
 #include<iostream>    //cout
 #include<stdio.h> //printf
-#include<string.h>    //strlen
 #include<string>  //string
 #include<sys/socket.h>    //socket
 #include<arpa/inet.h> //inet_addr
